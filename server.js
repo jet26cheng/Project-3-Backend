b9be61f69b57cec8323adb4590f3ffd5df8621fc
chengu <jeddycheng@gmail.com>
2023-01-09 10:24:01 -0800

    cors edit 18

--- server.js
+++ server.js
@@ -1,70 +1,59 @@
+require("./config/db.connection")
 
-require("./config/db.connection");
-
-require("dotenv").config();
-
+require("dotenv").config()
 
 // pull PORT from .env, give default value of 4000 and establish DB Connection
 const { PORT, MONGODB_URI } = process.env;
 
-// importing 
-const morgan = require("morgan")
+// importing
+const morgan = require("morgan");
 
 const cors = require("cors");
 
 const express = require("express");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-  app.use(cors())
-  
-  app.use(morgan("dev"));
+app.use(cors());
 
-
-app.use((req, res, next) => { 
-
-	console.log('I run for all routes');    
-	next();
-});
-
-app.use((req, res, next) => {    
-	console.log(`${req.method} ${req.originalUrl}`);    
-	next();
-}); 
+app.use(morgan("dev"));
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString() //this is method and we need to call that
-    next()
-  })
-  
-const chatController = require("./Controllers/chatcontroller.js")
+  console.log("I run for all routes");
+  next();
+});
 
-const userController = require("./Controllers/usercontroller.js")
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString(); //this is method and we need to call that
+  next();
+});
+
+const chatController = require("./Controllers/chatcontroller.js");
+
+const userController = require("./Controllers/usercontroller.js");
 
 const messageContoller = require("./Controllers/messagecontroller.js");
 
-// api routes 
+// api routes
 
-app.use("/chat", chatController)
+app.use("/chat", chatController);
 app.use((err, req, res, next) => res.status(500).send(err));
 
-app.use("/user", userController)
+app.use("/user", userController);
 app.use((err, req, res, next) => res.status(500).send(err));
 
-app.use("/message", messageContoller)
+app.use("/message", messageContoller);
 app.use((err, req, res, next) => res.status(500).send(err));
-
 
 app.all("/*", function (req, res) {
-    return res.status(404).json({errata: 
-    "No Resource Found Genius!" })
-  })
-  
-
-
-
-
+  return res.status(404).json({ errata: "No Resource Found Genius!" });
+});
 
 app.listen(PORT, () => console.log(`listening on PORT ${PORT}`));
