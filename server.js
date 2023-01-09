a588449275b8e81c08e037207dfb834993a2fdc8
chengu <jeddycheng@gmail.com>
2023-01-08 18:44:12 -0800

    cors edit 7

--- server.js
+++ server.js
@@ -45,6 +45,15 @@ app.use(express.json())
 
 app.use(cors())
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content -Type, Accept");
+    next();
+
+});
+
+
 app.options('*',cors())
 
 
