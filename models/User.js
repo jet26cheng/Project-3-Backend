cdf44b03281c61007898aa5a2964a5983f399a8f
chengu <jeddycheng@gmail.com>
2023-01-09 09:34:19 -0800

    cors edit 12

--- models/User.js
+++ models/User.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
     }],
     image: {
         type: String,
-        default:"../image/Default-Image.jpeg"
+        default:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fstock.adobe.com%2Fsearch%2Fimages%3Fk%3Ddefault%2Bprofile%2Bpicture&psig=AOvVaw2UQ3TWF5b0gwPmeQzg3B_a&ust=1673371938114000&source=images&cd=vfe&ved=0CA4QjRxqFwoTCKjj-rCCu_wCFQAAAAAdAAAAABAE"
     },
     displayname: {
         type: String,
